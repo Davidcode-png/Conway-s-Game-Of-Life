4554c77371403c66c45b92b6c918780599d80c5b
Opeoluwa <opeoluwa313@gmail.com>
2023-10-27 20:51:18 +0100

    Commit 1.2: Building Board

--- src/main.rs
+++ src/main.rs
@@ -46,7 +46,7 @@ fn main() {
         display(count, &mut map);
         count += 1;
         println!("Generation: {}", count);
-        thread::sleep(Duration::from_millis(160));
+        thread::sleep(Duration::from_millis(100));
         clearscreen::clear().expect("Failed to clear screen");
     }
 }
@@ -56,17 +56,19 @@ fn display(count: u64,  map: &mut [[u8; 50]; 50]){
     let squares = SquareSymbols::new();
     
     //let mut map: [[u8; 50]; 50] = [[0; 50]; 50];
-    
+    map[1 as usize][2 as usize] = 1;
+    map[2 as usize][1 as usize] = 1; 
+    map[0 as usize][1 as usize] = 1; 
     //let mut count: i32 = 0;
-    let mut row: i32 = 0;
-    let mut col: i32 = 0;
+    let mut row: i32;
+    let mut col: i32;
     for i in 0..50 {
         for j in 0..50 {
             // map[i][j] = (count as u8 % 2);
             //map[i][j] = rand::thread_rng().gen_range(0..2); 
             col = count as i32 % 50;
-            row = count as i32 / 49; 
-            map[row as usize][col as usize] = 1;
+            row = (count as i32 / 50) % 50; 
+         //   map[row as usize][col as usize] = 1;
 
         }
     }
